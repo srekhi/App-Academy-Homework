dbd2a7fcd3332ae0801f683c35aa545aa1af46f9
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-03-30 20:57:35 -0700

    Refactor map class

--- map.rb
+++ map.rb
@@ -1,42 +1,70 @@
 class Map
   #map stores information in key-value pairs.
   def initialize
-    @store = [[]]
+    @store = []
   end
 
   def assign(key, val)
-    found_key = false
-    @store.each do |pair|
-      if pair.first == key
-        pair[1] = val
-        found_key = true
-      end
-    end
-    unless found_key
-      @store.first.empty? ? @store = [[key, val]] : @store << [key, val]
-    end
+    key_index = @store.find_index { |pair| pair.first == key }
+    key_index ? @store[key_index] = [key, val] : @store << [key, val]
   end
 
   def lookup(key)
-    @store.each do |pair|
-      return pair.last if pair.first == key
-    end
+    @store.each { |pair| return pair.last if pair.first == key }
     nil
   end
 
+  def remove(key)
+    #instead of iterating through to find the index, then deleting it. just reject the index
+    #where the condition fits.
+    @store.reject! { |pair| pair.first == key }
+  end
+  
   def show
     @store
   end
 
 end
-# Test cases:
-# map = Map.new
-# map.assign(3, 4)
-# map.assign(4,5)
-# map.assign(2,5)
+
+
+# class Map
+#   def initialize
+#     @map = []
+#   end
 #
-# p map.show
+#   def assign(key, value)
+#     pair_index = @map.index {|pair| pair[0] == key}
+#     pair_index ? @map[pair_index][1] = value : @map.push([key, value])
+#     [key, value]
+#   end
 #
-# map.assign(2,1000) #should replace 2 with 1000
-# p "After update:"
-# p map.show
+#   def lookup(key)
+#     @map.each {|pair| return pair[1] if pair[0] == key}
+#     nil
+#   end
+#
+#   def remove(key)
+#     @map.reject! {|pair| pair[0] == key}
+#     nil
+#   end
+#
+#   def show
+#     deep_dup(@map)
+#   end
+#
+#   private
+#   def deep_dup(arr)
+#     arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+#   end
+# end
+#Test cases:
+map = Map.new
+map.assign(3, 4)
+map.assign(4,5)
+map.assign(2,5)
+
+p map.show
+
+map.assign(2,1000) #should replace 2 with 1000
+p "After update:"
+p map.show

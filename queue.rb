ba63924a53b2b7c069589439fbfb056394c6b641
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-03-30 20:59:38 -0700

    Refactor queue class

--- queue.rb
+++ queue.rb
@@ -18,12 +18,12 @@ class Queue
 
 end
 
-queue = Queue.new
-queue.enqueue(3) #first one in, so when I remove this should be the one that's out
-queue.enqueue(4)
-queue.enqueue(5)
-
-p queue.show
-
-queue.dequeue
-p queue.show
+# queue = Queue.new
+# queue.enqueue(3) #first one in, so when I remove this should be the one that's out
+# queue.enqueue(4)
+# queue.enqueue(5)
+#
+# p queue.show
+#
+# queue.dequeue
+# p queue.show

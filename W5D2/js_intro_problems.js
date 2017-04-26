82781967cf6c1e0067768d90482d50e1be978dc7
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-04-25 22:58:41 -0700

    Finish hwk

--- W5D2/js_intro_problems.js
+++ W5D2/js_intro_problems.js
@@ -86,3 +86,76 @@ console.log(sumOfNPrimes(1));
 
 console.log(sumOfNPrimes(4));
 // 17
+
+
+// Callbacks:
+function printCallBack(name_array) {
+  name_array.forEach(function(name){
+    return console.log(name);
+  });
+}
+function titleize(name_array, callBack) {
+  let new_names = name_array.map(function(name) {
+    return `Mx. ${name} Jingleheimer Schmidt`;
+  });
+  callBack(new_names);
+}
+titleize(["Mary", "Brian", "Leo"], printCallBack);
+
+// constructors, etc:
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() { console.log(`${this.name} the elephant goes phhRRRRRR`); };
+Elephant.prototype.grow = function () { this.height += 12; };
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+ };
+
+Elephant.prototype.play = function(){
+  let i = Math.random(0, this.tricks.length );
+  return this.tricks[i];
+};
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+// Why doesn't this work?
+Elephant.prototype.paradeHelper = function(elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+herd.forEach(function(elephant) {
+   return elephant.paradeHelper(elephant);
+});
+
+// herd.forEach(paradeHelper);
+
+// console.log(ellie.trumpet());
+// console.log(ellie.height);
+// ellie.grow();
+// console.log(ellie.height);
+//
+// console.log(ellie.tricks);
+// ellie.addTrick("rap");
+// console.log(ellie.tricks);
+
+function dinerBreakfast(orderItem) {
+  let order = "I'd like scrambled eggs and bacon";
+
+  function addOrder(orderItem) {
+    order += ` and ${orderItem}`;
+    console.log(order);
+  }
+  return addOrder;
+}
+let bfastOrder = dinerBreakfast();
+bfastOrder("chocolate chip pancakes");
+bfastOrder("grits");
